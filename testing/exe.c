11d2b39af1f7d11e31034de538a799529ea9f0e3
Badriah Alnahdi <balnahdi@lab3r8s12.42abudhabi.ae>
2022-07-03 18:54:10 +0400

    multi pipes

--- testing/exe.c
+++ testing/exe.c
@@ -1,4 +1,7 @@
 #include "../mini_chan.h"
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
 
 void path(t_shell_chan *main,char *av[],int argc)
 {
@@ -97,24 +100,159 @@ void ft_pipe(t_shell_chan *main,char *av[],int ac)
 	}
 	//?? **cmd_path(command path) && ***arg(cmd line)
 	//todo init forks and pipe
-	int ends[2];
-	if(pipe(ends) )
-	//pipes end [0]read .. end[1]write
-	/*
-		stdout close
-	*/
-	i = 0;
-	j = 0;
-	while(i < num_cmd)
+	// int ends[num_cmd][2]; // 0 read 1 write
+	// int st;
+	// i = 0;
+	// while(i < num_cmd)
+	// {
+	// 	if(pipe(ends[i])< 0)
+	// 		perror("Error pipes ");
+	// 	i++;
+	// }
+	// THE CREATION OF CHILD PROCESS AND EXE CHILD PROCESS SHOULD BE IN LOOP .. pid_t array
+	// i = 0;
+	// pid_t *child =(pid_t *)malloc(sizeof(pid_t) * num_cmd);
+	// while(i < num_cmd)
+	// {
+	// 	child[i] = fork();
+	// 	i++;
+	// }
+	// i = 0;
+	// while(i < num_cmd)
+	// {
+	// 	if(child[i] == 0) // one dup2 if child == 0 || child == num_cmd -1 
+	// 	{
+	// 		if(i == 0)
+	// 		{
+	// 			// printf("child1 %d  cmd :%s arg:%s\n",i,cmd_path[i], arg[i][0]);
+	// 			close(ends[i][0]);
+	// 			if(dup2(ends[i][0],STDIN_FILENO) < 0)
+	// 				perror("STD 1");
+	// 			close(ends[i][1]);
+	// 			execve(cmd_path[i],arg[i], NULL);
+	// 		}
+	// 		else if(i == num_cmd - 1)
+	// 		{
+	// 			printf("child last %d\n", i);
+	// 		}
+	// 		else
+	// 		{
+	// 			// printf("child2 %d\n",i);
+	// 			close(ends[i][1]);
+	// 			if(dup2(STDOUT_FILENO,STDIN_FILENO) < 0)
+	// 				perror("STD OUT IN");
+	// 			if(dup2(ends[i][0],STDOUT_FILENO) < 0)
+	// 				perror("STD 2");
+	// 			close(ends[i][0]);
+	// 			execve(cmd_path[i],arg[i], NULL);
+	// 		}
+	// 	}
+	// 	else
+	// 	 	perror("child error ");
+	// 	// waitpid(1,&st,0);
+	// 	i++;
+	// }
+	// int ends[2];
+	// pipe(ends);
+	// pid_t child1 = fork();
+	// int st;
+	// if(child1 == 0)
+	// {
+	// 	close(ends[0]); // ? should i close all other ends for other child process ?
+	// 	if(dup2(ends[1],STDOUT_FILENO) < 0)
+	// 		perror("CH1 STDOUT");
+	// 	close(ends[1]);
+	// 	execve(cmd_path[0], arg[0] , NULL);
+	// }
+
+
+	// pid_t child2;
+	// child2 = fork();
+	// if(child2 == 0)
+	// {
+	// 	close(ends[1]); // ? should i close all other ends for other child process ?
+	// 	if(dup2(ends[0],STDIN_FILENO) < 0)
+	// 		perror("CH1 STDOUT");
+	// 	close(ends[0]);
+	// 	execve(cmd_path[1], arg[1] , NULL);
+	// }
+	// waitpid(1,&st,0);
+	int ends[3][2];
+	pipe(ends[0]);
+	pipe(ends[1]);
+	pid_t child1 = fork();
+	int st;
+	if(child1 == 0)
 	{
-		j=0;
-		while(arg[i][j])
-		{
-			printf("%s\n",arg[i][j]);
-			j++;
-		}
-		i++;
+		close(ends[0][1]); // ? should i close all other ends for other child process ?
+		if(dup2(ends[0][0],STDIN_FILENO) < 0)
+			perror("CH1 STDOUT");
+		if(dup2(ends[1][1],STDOUT_FILENO) < 0)
+				perror("CH1 STDOUT");		
+		close(ends[0][0]);
+		close(ends[0][1]);
+		close(ends[1][0]);
+		close(ends[1][1]);
+		close(ends[2][0]);
+		close(ends[2][1]);
+		execve(cmd_path[0], arg[0] , NULL);
 	}
+	else{
+		pid_t child2 = fork();
+		if(child2 == 0)
+		{
+			// close(ends[][0]); // ? should i close all other ends for other child process ?
+			// close
+			if(dup2(ends[1][0],STDIN_FILENO) < 0)
+				perror("CH2 STDOUT");
+			if(dup2(ends[2][1],STDOUT_FILENO) < 0)
+				perror("CH2 STDOUT");
+			close(ends[0][0]);
+			close(ends[0][1]);
+			close(ends[1][0]);
+			close(ends[1][1]);
+			close(ends[2][0]);
+			close(ends[2][1]);
+			if(execve(cmd_path[1], arg[1] , NULL) == -1)
+				perror("exe ");
+		}
+		else {
+			pid_t child3;
+			child3 = fork();
+			if(child3 == 0)
+			{
+				// close(ends[1]); // ? should i close all other ends for other child process ?
+				printf("here \n");
+				if(dup2(ends[2][0],STDIN_FILENO) < 0)
+					perror("CH3 STDOUT");
+				close(ends[0][0]);
+				close(ends[0][1]);
+				close(ends[1][0]);
+				close(ends[1][1]);
+				close(ends[2][0]);
+				close(ends[2][1]);
+				if(execve(cmd_path[2], arg[2] , NULL) == -1)
+					perror(" exe 3");
+			}			
+		
+		
+		}
+	}	
+	waitpid(1,&st,0);
+	waitpid(1,&st,0);
+	// waitpid(1,&st,0);
+	// i = 0;
+	// j = 0;
+	// while(i < num_cmd)
+	// {
+	// 	j=0;
+	// 	while(arg[i][j])
+	// 	{
+	// 		printf("%s\n",arg[i][j]);
+	// 		j++;
+	// 	}
+	// 	i++;
+	// }
 	
 	
 }
